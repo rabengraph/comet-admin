cd9bff6f2fe33d0d9f47c9d383d1a3d7536da3c1
Simon Radner <simon.radner@gmail.com>
2021-05-18 11:21:30 +0000

    Test commit 2

--- packages/admin-rte/src/core/Rte.tsx
+++ packages/admin-rte/src/core/Rte.tsx
@@ -261,6 +261,7 @@ const Rte: React.RefForwardingComponent<any, IProps> = (props, ref) => {
 
     return (
         <div ref={editorWrapperRef} className={rootClasses.join(" ")}>
+            <h1> TEST PR 2</h1>
             <Controls editorRef={editorRef} editorState={editorState} setEditorState={onChange} options={options} disabled={disabled} />
             <div className={classes.editor}>
                 <DraftJsEditor
